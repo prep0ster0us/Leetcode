0028d6991f7838efa5dcd7a650cbf1e5cb4de98b
prep0ster0us <rbabu1@unh.newhaven.edu>
2023-12-11 23:15:49 -0500

    Dec 12, 23

---
+++ sorting/1464.cpp
@@ -0,0 +1,15 @@
+/********************************************************
+// 1464. Maximum Product of Two Elements in an Array
+
+Given the array of integers nums, you will choose two different indices i and j of that array.
+Return the maximum value of (nums[i]-1)*(nums[j]-1). 
+
+*********************************************************/
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        return (nums[0]-1)*(nums[1]-1);
+    }
+};
